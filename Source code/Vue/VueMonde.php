ec778506047bc37f461d4c5c4d7d2f49f812b223
bapcrespin <bap.crespin@gmail.com>
2016-03-31 18:16:34 +0200

    ajout parcours 3 et 4

--- Source code/Vue/VueMonde.php
+++ Source code/Vue/VueMonde.php
@@ -10,7 +10,7 @@
     <div class="container">
       &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp<a type="button" href="./index.php?entite=parcours1" class="btn btn-primary btn-lg"><div ><h1 class ='mafonte2'> Les Secondes</h1></div></a> &nbsp
       <a type="button" href="./index.php?entite=parcours2" class="btn btn-success btn-lg"> <div class ='mafonte2'><h1> Les Premières </h1></div></a> &nbsp
-     <a type="button" href="Vue/Parcours.php" class="btn btn-danger btn-lg"><div class ='mafonte2'><h1> Les Terminales </h1></div></a> &nbsp
-      <a type="button" href="Vue/Parcours.php" class="btn btn-info btn-lg"><div class ='mafonte2'><h1> Le Bac </h1></div></a>
+     <a type="button" href="./index.php?entite=parcours3" class="btn btn-danger btn-lg"><div class ='mafonte2'><h1> Les Terminales </h1></div></a> &nbsp
+      <a type="button" href="./index.php?entite=parcours4" class="btn btn-info btn-lg"><div class ='mafonte2'><h1> Le Bac </h1></div></a>
       
 
